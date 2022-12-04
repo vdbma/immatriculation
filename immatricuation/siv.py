430489b479c33d31e6574163e58677a81483325a
Marc Van den Bossche <marc.vanden-bossche@univ-grenoble-alpes.fr>
2022-12-04 21:30:38 +0100

    Working value computation

--- immatricuation/siv.py
+++ immatricuation/siv.py
@@ -1,6 +1,31 @@
 import re
 
 forbidden_letters = ["I", "O", "U"]
+alphabet = {
+    "A": 0,
+    "B": 1,
+    "C": 2,
+    "D": 3,
+    "E": 4,
+    "F": 5,
+    "G": 6,
+    "H": 7,
+    "J": 8,
+    "K": 9,
+    "L": 10,
+    "M": 11,
+    "N": 12,
+    "P": 13,
+    "Q": 14,
+    "R": 15,
+    "S": 16,
+    "T": 17,
+    "V": 18,
+    "W": 19,
+    "X": 20,
+    "Y": 21,
+    "Z": 22,
+}
 
 
 def isValid(immatriculation):
@@ -25,3 +50,28 @@ def isValid(immatriculation):
             return False
 
     return True
+
+
+def computeValue(immatriculation: str):
+    if not isValid(immatriculation):
+        return None
+    else:
+        first_letters, number, last_letters = [_.upper() for _ in immatriculation.split("-")]
+
+        value = int(number)
+
+        value += alphabet[last_letters[1]] * 999
+        value += alphabet[last_letters[0]] * len(alphabet) * 999
+
+        if last_letters > "SS":
+            value -= 999
+
+        value += alphabet[first_letters[1]] * 999 * (len(alphabet) ** 2 - 1)
+        value += alphabet[first_letters[0]] * 999 * (len(alphabet) ** 2 - 1) * len(alphabet)
+
+        if first_letters >= "SS":
+            value -= 999 * (23 * 23 - 1)
+        if first_letters >= "WW":
+            value -= 999 * (23 * 23 - 1)
+
+        return value
--- tests/test_siv.py
+++ tests/test_siv.py
@@ -1,4 +1,4 @@
-from immatricuation.siv import isValid
+from immatricuation.siv import isValid, computeValue
 
 
 def test_isValid():
@@ -12,3 +12,21 @@ def test_isValid():
     assert not isValid("ia-111-aa")
     assert not isValid("aa-111-ua")
     assert not isValid("aa-111-oa")
+
+
+def test_computeValue():
+    assert computeValue("aa-001-aa") == 1
+    assert computeValue("aa-999-aa") == 999
+
+    assert computeValue("aa-001-ab") == 1000
+    assert computeValue("aa-999-ab") == 1998
+    assert computeValue("aa-001-ba") == 999 * 23 + 1
+    assert computeValue("aa-001-ca") == 999 * 23 * 2 + 1
+    assert computeValue("aa-999-zz") == 999 * (23 * 23 - 1)
+
+    assert computeValue("ab-001-aa") == 999 * (23 * 23 - 1) + 1
+    assert computeValue("ac-001-aa") == 999 * (23 * 23 - 1) * 2 + 1
+    assert computeValue("ba-001-aa") == 999 * (23 * 23 - 1) * 23 + 1
+    assert computeValue("ca-001-aa") == 999 * (23 * 23 - 1) * 23 * 2 + 1
+
+    assert computeValue("zz-999-zz") == (23 * 23 - 2) * 999 * (23 * 23 - 1)
